4f74e25adbfa36aeee8c8863925bec5e3fd73336
neuromancer <x@y.z>
2014-03-22 21:49:47 -0300

    Fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,16 +6,23 @@ BOX_URL="http://www.cifasis-conicet.gov.ar/~gaa/package.box"
 #BUGS_URL="http://sourceforge.net/projects/ocean-data/files/debian_bugs.tar.gz/download" 
 #BOX_URL="http://sourceforge.net/projects/ocean-data/files/package.box/download"
 
-mkdir -p bugs
-wget "$BUGS_URL" -O "debian_bugs.tar.gz"
-mv "debian_bugs.tar.gz" bugs
-cd bugs
-tar -xf "debian_bugs.tar.gz"
-echo "Uncompressing bugs"
-for i in *.tar.bz2 ; do
-  printf "."
-  tar -xf "$i"
-done
-printf "\n"
-cd ..
-#vagrant box add "debian-7.3" "$BOX_URL"
+if [ ! -d bugs ]; then
+  mkdir -p bugs
+  wget "$BUGS_URL" -O "debian_bugs.tar.gz"
+  mv "debian_bugs.tar.gz" bugs
+  cd bugs
+  tar -xf "debian_bugs.tar.gz"
+  echo "Uncompressing bugs"
+  for i in *.tar.bz2 ; do
+    printf "."
+    tar -xf "$i"
+  done
+  printf "\n"
+  cd ..
+
+fi
+
+vagrant box add "debian-7.3" "$BOX_URL"
+
+
+
